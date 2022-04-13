8606b000241484bd21f85af5ed47f01323a9b8ef
Christian Unger <54417601+christian-unger@users.noreply.github.com>
2022-04-13 20:19:46 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1>
+        <span>Create your own</span>
+        <br />
+        <span className="gradient">gradient fonts like a pro.</span>
+      </h1>
+    </main>
+  )
+}
