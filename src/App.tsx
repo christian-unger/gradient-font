6c154b721193637246e2a1ca11b064ea4729ef1a
Christian Unger <54417601+christian-unger@users.noreply.github.com>
2022-04-13 20:48:50 +0000

    updated component

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,36 @@ export default function App() {
       <h1>
         <span>Create your own</span>
         <br />
-        <span className="gradient">gradient fonts like a pro.</span>
+        <GradientText colors={["#ef4444", "#3b82f6"]}>gradient fonts like a pro.</GradientText>
       </h1>
     </main>
   )
 }
+
+const isValidColor = (color: string) => {
+  const style = new Option().style
+  style.color = color
+  return style.color !== ""
+}
+
+type GradientTextProps = {
+  children: React.ReactNode
+  colors: string[]
+}
+
+const GradientText = ({ children, colors }: GradientTextProps) => {
+  const validColors = colors.filter(isValidColor)
+  const validColorsString = validColors.toString()
+
+  React.useLayoutEffect(() => {
+    let root = document.documentElement
+    validColors.forEach((color, index) => {
+      root.style.setProperty(`--gradient-color-${index}`)
+    })
+  }, [validColorsString])
+  
+  return (
+    <span className={`gradient`}>{children}</span>
+  )
+}
+
